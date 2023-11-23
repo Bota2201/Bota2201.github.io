b4cde1bf6afe2d7d6e9668027a54f0f7aeee4b0b
Bota2201 <122087292+Bota2201@users.noreply.github.com>
2023-11-23 13:12:50 +0600

    Add files via upload

---
+++ Food/src/js/script.js
@@ -0,0 +1,351 @@
+window.addEventListener('DOMContentLoaded', () => {
+    //Табы
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent () {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                
+                }
+            });
+        }
+    });
+
+    //таймер
+
+    const deadline = '2022-07-5';
+
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        
+        if(t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        }
+              
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock (selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock(); //чтобы верстка не моргала
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    
+    function openModal () {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+    
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal () {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+    modalCloseBtn.addEventListener('click',
+        closeModal
+    );//если надо вызвать функцию после клика, то мы ее не вызываем, а просто указываем название
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+           closeModal(); //Если надо вызвать функцию после условия, то вызываем
+        } //при клике на поле за модальным окном, оно тоже будет закрываться
+    });  
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    //const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+
+    //Используем классы для карточек
+
+    class MenuCard {
+        constructor (src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element); 
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+            `;
+
+            this.parent.append(element);
+
+        }
+    }
+
+    // const div = new MenuCard();
+    // div.render();
+    //укороченный вариант
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render(); //удобно, если мы один раз его используем  
+
+    
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container',
+        'menu__item'
+    ).render();
+});
+//Если использовать toggle, то в html надо указать класс hide
+
+
+// function showTabContent(i = 0) {
+//     tabsContent[i].classList.add('show', 'fade');
+//     tabsContent[i].classList.remove('hide');
+//     tabs[i].classList.add('tabheader__item_active');
+// }
+
+// hideTabContent();
+// showTabContent();
+
+// tabsParent.addEventListener('click', (e) => {
+//     const target = e.target;
+
+//     if (target && target.classList.contains('tabheader__item')) {
+//         tabs.forEach((item, i) => {
+//             if(target == item) {
+//                 hideTabContent();
+//                 showTabContent(i);
+            
+//             }
+//         });
+//     }
+// });
+
+let question = prompt('Какое «официальное» название JavaScript?', '')
+if (question === 'ECMAScript') {
+    alert('Верно');
+} else {
+    alert('Не знаете? ECMAScript!');
+}
+
+let num = prompt('укажи число','');
+
+if(num > 0) {
+    alert(1);
+} else if (num < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+let result;
+
+if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
+}
+
+result = (a + b < 4) ? 'Мало' : 'Много'
+
+let message;
+
+if (login == 'Сотрудник') {
+  message = 'Привет';
+} else if (login == 'Директор') {
+  message = 'Здравствуйте';
+} else if (login == '') {
+  message = 'Нет логина';
+} else {
+  message = '';
+}
+ 
+message = (login == 'Сотрудник') ? 'Привет' :
+          (login == 'Директор') ? 'Здравствуйте' :
+          (login == '') ? 'Нет логина' : '';
+
+
+for (let i = 2; i <= 10; i++) {
+    console.log(i++);
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log( `number ${i}!` );
+  }
+
+let i = 0;
+while (i < 3) {
+    console.log(`number ${i}!`);
+    i++;
+}
+
+let num;
+
+do {
+  num = prompt("Введите число больше 100?", 0);
+} while (num <= 100 && num);
+
+let n;
+for(i = 0; i < n; i++) {
+    if ()
+}
+
+
+function checkAge(age) {
+    // if (age > 18) {
+    //   return true;
+    // } else {
+    //   return confirm('Родители разрешили?');
+    // }
+    return (age > 18) ? true : confirm('Родители разрешили?');
+  }
+
+  console.log(checkAge(19))
+
+function min(a, b) {
+    return Math.min(a, b);
+}
+console.log(min(1,6))
+
+function pow(x, n) {
+    return Math.pow(x, n);
+}
+console.log(pow(5, 2))
+
+
